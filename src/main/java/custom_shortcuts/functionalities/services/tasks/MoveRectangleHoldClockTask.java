bf83da6b3647a0feede97f1d18cf9a64b421bc14
OskarLukaszewski <oskar.lukaszewski132@gmail.com>
2023-07-28 16:55:26 +0200

    Slowed down the loop in MoveRectangleHoldClockTask to decrease required processing power.

--- src/main/java/custom_shortcuts/functionalities/services/tasks/MoveRectangleHoldClockTask.java
+++ src/main/java/custom_shortcuts/functionalities/services/tasks/MoveRectangleHoldClockTask.java
@@ -6,10 +6,12 @@ public class MoveRectangleHoldClockTask extends Task<Void> {
 	@Override
 	protected Void call() throws Exception {
 		long startTime = System.currentTimeMillis();
-		while (System.currentTimeMillis() - startTime < 1000)
+		while (System.currentTimeMillis() - startTime < 1000) {
 			if (isCancelled()) {
 				throw  new InterruptedException();
 			}
+			Thread.sleep(10);
+		}
 		return null;
 	}
 }
